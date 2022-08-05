eb824fb311f1a24762fb894c09a69d96698abcdf
lakshya-uiet-cse <99861003+lakshya-uiet-cse@users.noreply.github.com>
2022-08-05 22:33:24 +0530

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,25 @@
+const data = {
+  products: [
+    {
+      id: '1',
+      name: 'Medicine1',
+      price: 140,
+      image: 'https://ibb.co/jwVKtyg',
+    },
+    {
+      id: '2',
+      name: 'Medicine2',
+      price: 240,
+      image: ' https://ibb.co/YPmKJ0W ',
+
+    },
+    {
+      id: '3',
+      name: 'Medicine3',
+      price: 150,
+      image: 'https://ibb.co/CP9Gfc4',
+    },
+  ],
+};
+export default data;
+
